2306d8ad45393150a34def05d3b13a7fc53cc1b8
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2022-08-21 11:27:57 +0200

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,18 @@
+ans = input("What does cpu stand for?")
+print(ans)
+if ans == 'central processing unit':
+   print("Well done you guessed correctly.")
+else:
+    print("You guessed incorrectly")
+ans = input("what does IT stand for?")
+print(ans)
+if ans == 'information technology':
+   print("Well done, you guessed correctly.')
+else:
+    print("You guessed incorrectly.")
+ans = input("what does ml stand for?")
+print(ans)
+if ans == 'machine learning':
+   print("well done, you answered correctly.")
+else:
+     print("You answered incorrcetly.")
